bb2db17fbadb407918254014df15fd384c2fcac2
computersarecool <willy@willynolan.com>
2017-11-14 02:40:06 -0800

    Updates quad

--- src/projects/compute_shader/assets/shaders/full_screen_quad.vert
+++ src/projects/compute_shader/assets/shaders/full_screen_quad.vert
@@ -2,10 +2,10 @@
 
 void main()
 {
-    const vec4 vertex[] = vec4[] (vec4(-1.0, -1.0, 0.5, 1.0),
-                                  vec4( 1.0, -1.0, 0.5, 1.0),
-                                  vec4(-1.0,  1.0, 0.5, 1.0),
-                                  vec4( 1.0,  1.0, 0.5, 1.0));
+    const vec4 vertex[] = vec4[](vec4(-1.0, -1.0, 0.0, 1.0),
+                                 vec4( 1.0, -1.0, 0.0, 1.0),
+                                 vec4(-1.0,  1.0, 0.0, 1.0),
+                                 vec4( 1.0,  1.0, 0.0, 1.0));
 
     gl_Position = vertex[gl_VertexID];
 }
--- src/projects/compute_shader/src/main.cpp
+++ src/projects/compute_shader/src/main.cpp
@@ -1,4 +1,5 @@
 ﻿// A basic cube example
+# include <iostream>
 #include "glm/glm/gtc/matrix_transform.hpp"
 
 #include "base_app.h"
@@ -64,7 +65,7 @@ private:
 		// Setup what is neccesary to draw the cubes
 		m_cube_shader = Shader{ "../assets/shaders/cube.vert", "../assets/shaders/cube.frag" };
 
-		// Cube vertex attribute parameters
+		// Vertex attribute parameters
 		const GLuint elements_per_face{ 6 };
 		const GLuint position_index{ 0 };
 		const GLuint normal_index{ 1 };
@@ -77,12 +78,12 @@ private:
 		const GLuint position_offset_in_buffer{ 0 };
 		const GLuint normal_offset_in_buffer{ sizeof(GLfloat) * position_size };
 
-		// Cube vertex buffer attributes
+		// Vertex buffer attributes
 		const GLuint binding_index{ 0 };
 		const GLuint offset{ 0 };
 		const GLuint element_stride{ sizeof(GLfloat) * elements_per_face };
 
-		// Setup the cube VBO and its data store
+		// Setup VBO and its data store
 		const GLuint flags{ 0 };
 		glCreateBuffers(1, &m_cube_vbo);
 		glNamedBufferStorage(m_cube_vbo, sizeof(vertices), vertices, flags);
@@ -102,31 +103,44 @@ private:
 		glVertexArrayAttribBinding(m_cube_vao, normal_index, binding_index);
 
 		glVertexArrayVertexBuffer(m_cube_vao, binding_index, m_cube_vbo, offset, element_stride);
-		// NOTE: We are leaving the VAO bound
 
-		// Setup what is neccesary for the textures
+		// Create framebuffer
 		glCreateFramebuffers(1, &m_fbo);
 
-
+		// Create the color texture for the framebuffer
 		glCreateTextures(GL_TEXTURE_2D, 1, &m_color_texture);
-		glTextureStorage2D(m_color_texture, 1, GL_RGBA32F, m_fbo_size, m_fbo_size);
+		glTextureStorage2D(m_color_texture, 1, GL_RGB8, m_fbo_size, m_fbo_size);
+		glTextureParameteri(m_color_texture, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+		glTextureParameteri(m_color_texture, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
-		glCreateTextures(GL_TEXTURE_2D, 1, &m_temp_texture);
-		glTextureStorage2D(m_temp_texture, 1, GL_RGBA32F, m_fbo_size, m_fbo_size);
+		glNamedFramebufferTexture(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, m_color_texture, 0);
 
-		glFramebufferTexture(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, m_color_texture, 0);
+		static const GLenum draw_buffers[]{ GL_COLOR_ATTACHMENT0 };
+		glNamedFramebufferDrawBuffers(m_fbo, 1, draw_buffers);
+
+
+
+		glCreateVertexArrays(1, &m_full_screen_quad_vao);
+		glBindVertexArray(m_full_screen_quad_vao);
+
+		// Setup what is neccesary for the full screen quad
+		m_full_screen_quad_shader = Shader{ "../assets/shaders/full_screen_quad.vert", "../assets/shaders/full_screen_quad.frag" };
 	}
 
 	virtual void render(double current_time)
 	{
-		// Set OpenGL state
-		glViewport(0, 0, m_info.window_width, m_info.window_height);
+		// Setup cube framebuffer
+		glBindFramebuffer(GL_FRAMEBUFFER, m_fbo);
+		//check_gl_error();
+		std::cout << glCheckFramebufferStatus(GL_FRAMEBUFFER) << std::endl;
+		glViewport(0, 0, m_fbo_size, m_fbo_size);
 		glClearBufferfv(GL_COLOR, 0, m_clear_color);
-		glClearBufferfi(GL_DEPTH_STENCIL, 0, 1.0f, 0);
-		glEnable(GL_DEPTH_TEST);
-		glDepthFunc(GL_LEQUAL);
+		//glClearBufferfi(GL_DEPTH_STENCIL, 0, 1.0f, 0);
+		//glEnable(GL_DEPTH_TEST);
+		//glDepthFunc(GL_LEQUAL);
 
-		// Draw cubes
+		// Render cubes
+		glBindVertexArray(m_cube_vao);
 		m_cube_shader.use();
 		for (int i = 0; i < m_number_cubes; i++)
 		{
@@ -138,11 +152,29 @@ private:
 			m_cube_shader.set_mat4("uProjectionMatrix", m_camera.get_proj_matrix());
 			glDrawArrays(GL_TRIANGLES, 0, m_vertices_per_cube);
 		}
+
+		//// Setup default framebuffer
+		//glBindFramebuffer(GL_FRAMEBUFFER, 0);
+		//glViewport(0, 0, m_info.window_width, m_info.window_height);
+		//glClearBufferfv(GL_COLOR, 0, m_clear_color);
+		////glClearBufferfi(GL_DEPTH_STENCIL, 0, 1.0f, 0);
+		////glEnable(GL_DEPTH_TEST);
+		////glDepthFunc(GL_LEQUAL);
+		//
+		////glBindTextureUnit(0, m_color_texture);
+		//glBindTexture(GL_TEXTURE_2D, m_color_texture);
+
+		//// Render FBO
+		//m_full_screen_quad_shader.use();
+		//glBindVertexArray(m_full_screen_quad_vao);
+		//glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
 	};
 
 	// Member variables
 	Shader m_cube_shader;
+	Shader m_full_screen_quad_shader;
 	GLuint m_cube_vao;
+	GLuint m_full_screen_quad_vao;
 	GLuint m_cube_vbo;
 	GLuint m_fbo;
 	GLuint m_color_texture;
@@ -150,7 +182,7 @@ private:
 	Camera m_camera{ glm::vec3{ 0, 0, 5 } };
 	const GLuint m_vertices_per_cube{ 36 };
 	const int m_number_cubes{ 9 };
-	const int m_fbo_size{ 2048 };
+	const int m_fbo_size{ 800 };
 	const glm::vec3 m_world_up{ glm::vec3{ 0, 1, 0 } };
 	const GLfloat m_clear_color[4]{ 0.2f, 0.0f, 0.2f, 1.0f };
 };
